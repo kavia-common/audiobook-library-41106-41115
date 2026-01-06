df3c25370a16f70335058a50376790cdb44994a2
Kavia AI <production@kavia.ai>
2026-01-06 02:28:06 +0000

    Checkpoint 1 - cg202ff098

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/audiobook-library-41106-41115/audiobook_store_frontend
+flutter analyze
+
---
+++ audiobook_store_frontend/android/app/src/main/kotlin/com/example/audiobook_store_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.audiobook_store_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
